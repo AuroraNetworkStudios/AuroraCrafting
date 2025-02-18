5e61dcaf0273bb16930783019a475eb4f0cef608
Perwollnt <me@perwollnt.me>
2025-02-18 18:30:23 +0100

    Crafting recipe view configs

---
+++ src/main/java/gg/auroramc/crafting/config/menu/vanilla/SmokerRecipeViewConfig.java
@@ -0,0 +1,49 @@
+package gg.auroramc.crafting.config.menu.vanilla;
+
+import gg.auroramc.aurora.api.config.AuroraConfig;
+import gg.auroramc.aurora.api.config.premade.ItemConfig;
+import gg.auroramc.crafting.AuroraCrafting;
+import lombok.Getter;
+
+import java.io.File;
+import java.util.Map;
+
+import static gg.auroramc.crafting.config.ConfigManager.VANILLA_RECIPE_VIEW_PATH;
+
+@Getter
+public class SmokerRecipeViewConfig extends AuroraConfig {
+    private String title = "Smoker Recipes";
+    private Integer rows = 4;
+    private Map<String, ItemConfig> customItems;
+
+    private Items items;
+    private Slots slots;
+
+    @Getter
+    public static final class Slots {
+        private Integer result = 16;
+        private Integer fuel = 19;
+        private Integer input = 10;
+    }
+
+    @Getter
+    public static final class Items {
+        private ItemConfig filler;
+        private ItemConfig back;
+        private ItemConfig fuel;
+    }
+
+    public SmokerRecipeViewConfig(AuroraCrafting plugin) {
+        super(getFile(plugin));
+    }
+
+    public static File getFile(AuroraCrafting plugin) {
+        return new File(plugin.getDataFolder() + "/" + VANILLA_RECIPE_VIEW_PATH, "smoker.yml");
+    }
+
+    public static void saveDefault(AuroraCrafting plugin) {
+        if (!getFile(plugin).exists()) {
+            plugin.saveResource(VANILLA_RECIPE_VIEW_PATH + "/smoker.yml", false);
+        }
+    }
+}
