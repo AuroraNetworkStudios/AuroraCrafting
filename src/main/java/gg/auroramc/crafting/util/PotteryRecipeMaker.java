dfa42a1cbe79ea757b5917bc147e29e64b4bd029
Perwollnt <me@perwollnt.me>
2025-02-18 23:34:21 +0100

    Optimize and clean Firework recipe creator class and make vanilla item matcher function more readable

--- src/main/java/gg/auroramc/crafting/util/PotteryRecipeMaker.java
+++ src/main/java/gg/auroramc/crafting/util/PotteryRecipeMaker.java
@@ -10,34 +10,32 @@ import java.util.Arrays;
 import java.util.List;
 
 public class PotteryRecipeMaker {
+
+    private static final DecoratedPot.Side[] SHARD_SIDES = new DecoratedPot.Side[]{
+            DecoratedPot.Side.FRONT,
+            DecoratedPot.Side.BACK,
+            DecoratedPot.Side.LEFT,
+            DecoratedPot.Side.RIGHT,
+    };
+
     public static ItemStack create(ItemStack[] matrix) {
-
-        // split items into groups of 3
-        List<ItemStack[]> groups = splitIntoGroups(matrix, 3).reversed();
-
         ItemStack pot = new ItemStack(Material.DECORATED_POT);
         BlockStateMeta meta = (BlockStateMeta) pot.getItemMeta();
         DecoratedPot state = (DecoratedPot) meta.getBlockState();
 
-        state.setSherd(DecoratedPot.Side.FRONT, Material.BLADE_POTTERY_SHERD);
 
-        DecoratedPot.Side[] sides = new DecoratedPot.Side[] {
-                DecoratedPot.Side.FRONT,
-                DecoratedPot.Side.BACK,
-                DecoratedPot.Side.LEFT,
-                DecoratedPot.Side.RIGHT,
-        };
+        List<ItemStack[]> groups = splitIntoGroups(matrix);
+        int groupCount = groups.size();
+        int sideIndex = 0;
 
-        int i = 0;
-        for(ItemStack[] group : groups) {
-            for(ItemStack item : group) {
+        for(int i = groupCount - 1; i >= 0 && sideIndex < SHARD_SIDES.length; i--) {
+            for (ItemStack item : groups.get(i)) {
                 if(item.isEmpty()) continue;
 
-                if(i >= sides.length) break;
-
-                state.setSherd(sides[i], item.getType());
-                i++;
+                state.setSherd(SHARD_SIDES[sideIndex++], item.getType());
+                if(sideIndex >= SHARD_SIDES.length) break;
             }
+
         }
 
         meta.setBlockState(state);
@@ -48,14 +46,14 @@ public class PotteryRecipeMaker {
     }
 
 
-    private static List<ItemStack[]> splitIntoGroups(ItemStack[] matrix, int groupSize) {
-        List<ItemStack[]> groups = new ArrayList<>();
-        for (int i = 0; i < matrix.length; i += groupSize) {
-            ItemStack[] group = Arrays.copyOfRange(matrix, i, Math.min(matrix.length, i + groupSize));
-            groups.add(group);
+    private static List<ItemStack[]> splitIntoGroups(ItemStack[] matrix) {
+        int groupCount = (int) Math.ceil((double) matrix.length / 3);
+        List<ItemStack[]> groups = new ArrayList<>(groupCount);
+
+        for (int i = 0; i < matrix.length; i += 3) {
+            groups.add(Arrays.copyOfRange(matrix, i, Math.min(matrix.length, i + 3)));
         }
         return groups;
     }
 
 }
-
