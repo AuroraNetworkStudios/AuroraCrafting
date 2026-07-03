5ce060507462d7547009ceee074feba750c65b72
Muhammad Tamir <muhammad.tamir@gmail.com>
2026-07-03 19:05:50 +0700

    Remove CraftEngine Item Resolver

--- src/main/java/gg/auroramc/crafting/hooks/craftengine/CraftEngineHook.java
+++ src/main/java/gg/auroramc/crafting/hooks/craftengine/CraftEngineHook.java
@@ -1,6 +1,5 @@
 package gg.auroramc.crafting.hooks.craftengine;
 
-import gg.auroramc.aurora.api.AuroraAPI;
 import gg.auroramc.crafting.AuroraCrafting;
 import gg.auroramc.crafting.hooks.Hook;
 import net.momirealms.craftengine.bukkit.api.event.CraftEngineReloadEvent;
@@ -14,10 +13,6 @@ public class CraftEngineHook implements Hook, Listener {
     public void hook(AuroraCrafting plugin) {
         this.plugin = plugin;
         plugin.getItemLoader().addToWaitFor("CraftEngine", 400);
-
-        var resolver = new CraftEngineItemResolver();
-        AuroraAPI.getItemManager().registerResolver("craftengine", resolver);
-        AuroraAPI.getItemManager().registerResolver("craft-engine", resolver);
     }
 
     @EventHandler
